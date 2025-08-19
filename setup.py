5adcb8022e9b87f1b67147ee5fce315b48390bfe
Prachet Pandav <you@example.com>
2025-08-19 16:56:03 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="Medical_ChatBot",
+    version="0.1.0",
+    author="Prachet Pandav",
+    author_email="prachetpandav283@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
